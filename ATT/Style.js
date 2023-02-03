cfb53b9fb09d4e1428740493d31a46b99cf824cc
AzizbekObidov/ObidovR <azizbekobidjonov1@gmail.com>
2023-02-03 11:59:02 +0500

    ddd

--- ATT/Style.js
+++ ATT/Style.js
@@ -1,10 +1,16 @@
 let savollar = [
-    { id: 1, savol: 'Kompyuterning asosiy qurilmalari nimalardan iborat?', a: 'Sistemali blok, monitor, klaviatura, sichqoncha  ', b: 'Modem, printer, monitor, klaviatura ', c: 'Sistemali blok, printer, klaviatura, sichqoncha', d: 'A va B' },
+    { id: 1, savol: 'Kompyuterning asosiy qurilmalari nimalardan iborat?', a: 'Sistemali blok, monitor, klaviatura, sichqoncha  ', b: 'Modem, printer, monitor, klaviatura ', c: 'Sistemali blok, printer, klaviatura, sichqoncha' },
     { id: 2, savol: 'Monitor  so’zining  ma’nosini toping?.', a: 'Lotincha   “eslatuvchi, xabar  beruvchi” ', b: 'Inglizcha   eslatuvchi', c: 'Grekcha   eslatuvch ', d: 'Grekcha  “ eslatuvchi, xabar  beruvchi' },
     { id: 3, savol: 'Magnit discklariga axborot qaysi qurilma yordamida yoziladi?', a: 'Proector', b: 'Printer   ', c: 'Nurli pero', d: 'Disckyugurtich' },
     { id: 4, savol: 'Algoritim bu-nima deyiladi ?', a: 'ko’rsatmalar ', b: 'tartib buzilishi ', c: 'buyruqlarning ketma –ketligi ', d: 'mo’ljallangan narsa ' },
+    { id: 5, savol: 'Kibernetika maktabining asoschisi kim? ', a: 'Vasil Qobilov  ', b: 'Vasil Ahmedov ', c: ' Jordan ', d: 'Fleming  ' },
+    { id: 6, savol: 'Qachon 1-kompyuter yaratildi?', a: '1977 ', b: '1973 ', c: '1968 ', d: '1912 ' },
+    { id: 7, savol: 'Fayl so`zining manosi?', a: 'maydon', b: 'amal  ', c: 'lumot ', d: ' adres ' },
 ]
 
+let javob = []
+let javobvariant = ['a', 'b', 'c', 'd']
+let tugrijavoblar = ['a', 'a', 'd', 'c','a','b','c']
 let uyinchilar = [
     { id: 1, name: '' },
     { id: 2, name: 'Doniyorbek' },
@@ -26,10 +32,20 @@ let uyinchilardata = [
     { id: 4, name: 'Doniyorbek Tursunov4', strs: 6, img: './img/doniyor.jpg' },
     { id: 5, name: 'Doniyorbek Tursunov5', strs: 3, img: './img/doniyor.jpg' },
     { id: 6, name: 'Doniyorbek Tursunov5', strs: 3, img: './img/doniyor.jpg' },
+    { id: 7, name: 'Doniyorbek Tursunov5', strs: 3, img: './img/doniyor.jpg' },
+    { id: 8, name: 'Doniyorbek Tursunov5', strs: 1, img: './img/doniyor.jpg' },
+    { id: 9, name: 'Doniyorbek Tursunov5', strs: 3, img: './img/doniyor.jpg' },
+    { id: 10, name: 'Doniyorbek Tursunov5', strs: 5, img: './img/doniyor.jpg' },
+    { id: 11, name: 'Doniyorbek Tursunov5', strs: 3, img: './img/doniyor.jpg' },
+    { id: 12, name: 'Doniyorbek Tursunov5', strs: 6, img: './img/doniyor.jpg' },
 ];
-let javob = []
-let javobvariant = ['a', 'b', 'c', 'd']
-let tugrijavoblar = ['a', 'a', 'd', 'c']
+let ustozlardata = [
+    { id: 1, name: 'Aaaaaaaa Tttttttttt', img: './img/ustoz.png', strs: 6 },
+    { id: 2, name: 'Aaaaaaaa Tttttttttt', img: './img/ustoz.png', strs: 6 },
+    { id: 3, name: 'Aaaaaaaa Tttttttttt', img: './img/ustoz.png', strs: 6 },
+    { id: 4, name: 'Aaaaaaaa Tttttttttt', img: './img/ustoz.png', strs: 6 },
+    { id: 5, name: 'Aaaaaaaa Tttttttttt', img: './img/ustoz.png', strs: 6 },
+]
 
 
 
@@ -43,16 +59,17 @@ uyinchilardata.map((val, i) => (
 
 // uyinchi select
 uyincard.value = ''
+let uyinchiprofil = ''
 let profilcard = document.querySelector('.profilcard')
- uyincard.addEventListener  ('change', (e) => {
+uyincard.addEventListener('change', (e) => {
     console.log(e.target.value);
-    uyinchi =  uyinchilardata.filter((val)=>val.id === e.target.value?val.name:'')
-    let uyinchiprofil =  uyinchilardata.filter((val)=>val.id == e.target.value)
+    uyinchi = uyinchilardata.filter((val) => val.id === e.target.value ? val.name : '')
+    uyinchiprofil = uyinchilardata.filter((val) => val.id == e.target.value)
     uyinchiprofil = uyinchiprofil[0]
-        profilcard.innerHTML = `
+    profilcard.innerHTML = `
         <h2 class="title">${uyinchiprofil.name}  <img src=${uyinchiprofil.img} alt=""></h2>
     `
-    
+
 })
 
 
@@ -65,7 +82,7 @@ function uyinfunc() {
      <button  onclick='javobfun(${0})'>A)  <span>${savollar[savolcount].a}</span></button>
      <button   onclick='javobfun(${1})'>B)  <span>${savollar[savolcount].b}</span></button>
      <button  onclick='javobfun(${2})'>C)  <span>${savollar[savolcount].c}</span></button>
-     <button   onclick='javobfun(${3})'>D)   <span>${savollar[savolcount].d}</span></button>
+     ${savollar[savolcount].d ? `<button   onclick='javobfun(${3})'>D)   <span>${savollar[savolcount].d}</span></button>` : ''} 
    </div>
    <div class="uyinbodyfooter">
      <button class="next" onclick='savolcountfun()'>Next </button>
@@ -85,11 +102,12 @@ uyinfunc()
 /// ///////////////
 
 let b = 0;
-let secfun =  setInterval(secuntfun, 1000);
+let yakunresult = false
+let secfun = setInterval(secuntfun, 1000);
 clearInterval(secfun)
 function startfun() {
     uyinfunc()
-    secfun =  setInterval(secuntfun, 1000);
+    secfun = setInterval(secuntfun, 1000);
     b = 0
 }
 
@@ -99,20 +117,23 @@ function secuntfun() {
 }
 let savolcounttext = document.querySelector('.savolcount');
 let savolcount = 0;
+savolcounttext.innerHTML = savolcount + 1  +` / `+savollar.length
 function savolcountfun() {
-    if(savolcount < savollar.length -1 ){
+    if (savolcount < savollar.length - 1) {
         savolcount++
-    }else{
-        let a = setTimeout(yakunalert,300)
+    } else {
+        let a = setTimeout(yakunalert, 100)
         savolcount = 0
+        yakunresult = true
+        clearInterval(secfun)
     }
-    savolcounttext.innerHTML = savolcount +1
+    savolcounttext.innerHTML = savolcount + 1 +` / `+savollar.length
     uyinfunc()
 }
 // Clearcountfun
-function Clearcountfun(){
+function Clearcountfun() {
     savolcount = 0
-    savolcounttext.innerHTML = savolcount +1
+    savolcounttext.innerHTML = savolcount + 1  +` / `+savollar.length
     clearInterval(secfun)
     secund.innerHTML = 0
     uyinchi = ''
@@ -126,53 +147,63 @@ function javobfun(i) {
     let variant = document.querySelectorAll('.variant > button')
     javob.push(javobvariant[i])
 
-    if (javob.flat('').reverse().slice(0,1)[0] ===tugrijavoblar[savolcount]) {
+    if (javob.flat('').reverse().slice(0, 1)[0] === tugrijavoblar[savolcount]) {
         variant[i].classList.add('ok')
-        let a = setTimeout(okalert,300)
+        let a = setTimeout(okalert, 300)
     } else {
         variant[i].classList.add('err')
-        let a = setTimeout(erralert,300)
+        let a = setTimeout(erralert, 300)
     }
 }
 // ///uyinmodalfun
 // alertalr
-function okalert(){
-    let uyinmodal =   document.querySelector('.uyinmodal')
+function okalert() {
+    let uyinmodal = document.querySelector('.uyinmodal')
     let uyinmodalcard = document.querySelector('.uyinmodalcard')
-    uyinmodalcard.innerHTML=`
+    uyinmodalcard.innerHTML = `
     <img src="./img/ok.png" alt="">
     <button class='next' onclick='uyinmodalfun()'>Next</button>
     `
     uyinmodal.classList.add('active')
 }
-function erralert(){
-    let uyinmodal =   document.querySelector('.uyinmodal')
+function erralert() {
+    let uyinmodal = document.querySelector('.uyinmodal')
     let uyinmodalcard = document.querySelector('.uyinmodalcard')
-    uyinmodalcard.innerHTML=`
+    uyinmodalcard.innerHTML = `
       <img src="./img/err.png" alt="" class='errimg'>
-    <button class='next' onclick='uyinmodalfun()'>Next</button>
+      <button class='next' onclick='uyinmodalfun()'>Next</button>
     `
-        uyinmodal.classList.add('active')
+    uyinmodal.classList.add('active')
 }
-function yakunalert(){
-    let uyinmodal =   document.querySelector('.uyinmodal')
+function yakunalert() {
+    let uyinmodal = document.querySelector('.uyinmodal')
     let uyinmodalcard = document.querySelector('.uyinmodalcard')
-    let yakun = javob.filter((val,i)=> javob[i] === tugrijavoblar[i])
+    let yakun = javob.filter((val, i) => javob[i] === tugrijavoblar[i])
     let foiz = 100 / tugrijavoblar.length
-    uyinmodalcard.innerHTML=`
-        <h1 class='blacktitle'> <img src=${(foiz *yakun.length)>70? './img/yutuqimg.png':'./img/yutuqimg3.png'} alt='' />   ${foiz *yakun.length} % bajardingiz !!  <img src=${(foiz *yakun.length)>70? './img/yutuqimg2.png':'./img/yutuqimg3.png'} alt='' /></h1>
-        <button class='next' onclick='uyinmodalfunyakun()'>Next</button>
+    uyinmodalcard.innerHTML = `
+    <h1 class='blacktitle'> <img src=${Math.floor(foiz * yakun.length) > 70 ? './img/yutuqimg.png' : './img/yutuqimg3.png'} alt='' />   ${Math.floor(foiz * yakun.length)} %  ya'ni ${yakun.length} ta to'g'ri   bajardingiz !!  <img src=${(foiz * yakun.length) > 70 ? './img/yutuqimg2.png' : './img/yutuqimg3.png'} alt='' /></h1>
+        <div class='modalyacunprofil'>
+        <img src=${uyinchiprofil.img} alt="" class=''>
+        <h2 class="title">${uyinchiprofil.name}</h2>
+        </div>   
+        ${yakunresult ? `<button class='next' onclick='uyinmodalfunStart()'>Start</button>` : `<button class='next' onclick='uyinmodalfunyakun()'>Next</button>`}
     `
-        uyinmodal.classList.add('active')
-        secund.innerHTML = 0
+    uyinmodal.classList.add('active')
+    secund.innerHTML = 0
 }
-function uyinmodalfunyakun(){
-    let uyinmodal =   document.querySelector('.uyinmodal')
+function uyinmodalfunyakun() {
+    let uyinmodal = document.querySelector('.uyinmodal')
     uyinmodal.classList.remove('active')
 }
+// uyinmodalfunStart
+function uyinmodalfunStart() {
+    uyinfunc()
+    b = 0
+    secfun = setInterval(secuntfun, 1000);
+}
 // alertalr
-function uyinmodalfun(){
-    let uyinmodal =   document.querySelector('.uyinmodal')
+function uyinmodalfun() {
+    let uyinmodal = document.querySelector('.uyinmodal')
     uyinmodal.classList.remove('active')
     savolcountfun()
 }
@@ -181,17 +212,18 @@ function uyinmodalfun(){
 
 // uyinchilar
 let uyinchilarcards = document.querySelector('.uyinchilarcards')
+let uyinchilarcards2 = document.querySelector('.uyinchilarcards2')
+let ustozlarcard = document.querySelector('.ustozlarcard')
 function uyinchilarfun() {
-    uyinchilardata.map((val, i) => {
+    uyinchilardata.filter((val) => val.strs >= 4).map((val, i) => {
         uyinchilarcards.innerHTML += `
         <div class="uyinchicard">
         <div class="imgcard"><img src="./img/doniyor.jpg" alt="" class="img"></div>
         <div class="satrs">
-           ${
-              Array(val.strs).fill('').map((img)=>(
-                 `<img src='./img/star.png' alt=""/>`
-              ))
-           }
+           ${Array(val.strs).fill('').map((img) => (
+            `<img src='./img/star.png' alt=""/>`
+        ))
+            }
         </div>
         <h3 class="name">${val.name}</h3>
     </div>
@@ -200,5 +232,43 @@ function uyinchilarfun() {
 
 }
 uyinchilarfun()
+function uyinchilarfun2() {
+    uyinchilardata.filter((val) => val.strs < 4).map((val, i) => {
+        uyinchilarcards2.innerHTML += `
+        <div class="uyinchicard">
+        <div class="imgcard"><img src="./img/doniyor.jpg" alt="" class="img"></div>
+        <div class="satrs">
+           ${Array(val.strs).fill('').map((img) => (
+            `<img src='./img/star.png' alt=""/>`
+        ))
+            }
+        </div>
+        <h3 class="name">${val.name}</h3>
+    </div>
+        `
+    })
+
+}
+uyinchilarfun2()
+
+
+function ustozlarfunfun() {
+    ustozlardata.map((val, i) => {
+        ustozlarcard.innerHTML += `
+        <div class="uyinchicard">
+        <div class="imgcard"><img src=${val.img} alt="" class="img"></div>
+        <div class="satrs">
+           ${Array(val.strs).fill('').map((img) => (
+            `<img src='./img/star.png' alt=""/>`
+        ))
+            }
+        </div>
+        <h3 class="name">${val.name}</h3>
+    </div>
+        `
+    })
+
+}
+ustozlarfunfun()
 
 
